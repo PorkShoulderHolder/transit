e28469bd99249bc782cc24ed274d8167d14a9a5b
Ubuntu <ubuntu@ip-10-221-29-228.us-west-2.compute.internal>
2016-08-07 17:09:04 -0400

    remote changes

--- cron_setup.sh
+++ cron_setup.sh
@@ -1,4 +1,6 @@
-mkdir $(pwd)/transit/processed_data
+mkdir $(pwd)/transport/processed_data
 mkdir $(pwd)/mta/mta_data
+echo "added storage directories"
 echo "0 5 */2 * * python "$(pwd)"/mta_api/run.py > /dev/null" > crontab.txt
 crontab $(pwd)/crontab.txt
+echo "created and initiated cronjob"
