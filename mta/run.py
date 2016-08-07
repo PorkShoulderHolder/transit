e7cd640fffd6ebfaf0c561af5fac17d54bd4db15
Sam Royston <sfoxroyston@gmail.com>
2016-08-07 14:49:47 -0400

    objectified

--- mta/run.py
+++ mta/run.py
@@ -3,8 +3,8 @@ from turnstile_synch import UpdateManager
 from transit import transit
 
 update_manager = UpdateManager(start_yr=15)
-# update_manager.synch_turnstiles()
-# update_manager.synch_locations()
-# update_manager.synch_gtfs()
+update_manager.synch_turnstiles()
+update_manager.synch_locations()
+update_manager.synch_gtfs()
 
 transit.run_opts()
