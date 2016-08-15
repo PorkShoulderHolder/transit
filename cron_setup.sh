cdd88e51eed26339b363e194fd4105110a9e6e59
Sam Royston <sfoxroyston@gmail.com>
2016-08-14 23:24:30 -0400

    local

--- cron_setup.sh
+++ cron_setup.sh
@@ -1,6 +1,6 @@
 mkdir $(pwd)/transport/processed_data
 mkdir $(pwd)/mta/mta_data
 echo "added storage directories"
-echo "0 5 */2 * * ."$(pwd)"/run.sh" > crontab.txt
+echo "0 5 */2 * * . "$(pwd)"/run.sh" > crontab.txt
 crontab $(pwd)/crontab.txt
 echo "created and initiated cronjob"
