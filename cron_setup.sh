9d7a2d57816c3a28ca75d94dc1ebc7b61708ab8c
Sam Royston <sfoxroyston@gmail.com>
2016-08-14 22:50:14 -0400

    reload

--- cron_setup.sh
+++ cron_setup.sh
@@ -1,6 +1,6 @@
 mkdir $(pwd)/transport/processed_data
 mkdir $(pwd)/mta/mta_data
 echo "added storage directories"
-echo "0 5 */2 * * python "$(pwd)"/mta_api/run.py > /dev/null" > crontab.txt
+echo "0 5 */2 * * sh "$(pwd)"/run.sh" > crontab.txt
 crontab $(pwd)/crontab.txt
 echo "created and initiated cronjob"
